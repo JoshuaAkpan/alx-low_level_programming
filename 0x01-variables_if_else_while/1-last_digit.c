a09e3c7d35e5cda925f4df29a46c808e56d201c3
JoshuaAkpan <joshuaakpan7@gmail.com>
2022-09-09 15:14:57 -0700

    The file 1-last_digit.c contains a program that will assign a random number to the variable n each time it is executed. Then print the last digit of the number stored in the variable n.

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	d = n % 10;
+
+	/* your code goes there */
+
+	if (d > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, d);
+	}
+	else if (d == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, d);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
+	}
+
+	return (0);
+}
