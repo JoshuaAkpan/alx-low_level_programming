58d6c5dae7c23f7d7ccb6543c541855d0e2c717b
JoshuaAkpan <joshuaakpan7@gmail.com>
2022-09-08 17:09:50 -0700

    updated to betty documentation style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char my_str[] = "\"Programming is like building a multilingual puzzle";
