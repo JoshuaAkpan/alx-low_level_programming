d8a8cc7979801708e9d8f7beaa6ea82588eb3178
JoshuaAkpan <joshuaakpan7@gmail.com>
2022-09-13 17:25:25 -0700

    corrected syntax errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- *
- * this function prints the alphabet, in lowercase, followed by a new line.
+ * Description: This prints the alphabet, in lowercase.
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char a;
-	
+
 	for (a = 'a'; a <= 'z'; a++)
 	{
 		_putchar(a);
 	}
 	_putchar('\n');
-
-	return;
 }
