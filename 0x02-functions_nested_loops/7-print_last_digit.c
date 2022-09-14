7025fafc226f2eb5b62a20ad18e5864c1e5d156b
JoshuaAkpan <joshuaakpan7@gmail.com>
2022-09-13 20:59:32 -0700

    This commit adds a function that prints the last digit of a number..... It also updates the header file (main.h)

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ *
+ * @n: parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	int lstdig = n % 10;
+
+	return (lstdig);
+}
