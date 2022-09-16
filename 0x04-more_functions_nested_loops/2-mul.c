248bbea26473cfac3a836b6aa72a8f36ba5ad250
JoshuaAkpan <joshuaakpan7@gmail.com>
2022-09-15 18:00:43 -0700

    This commit updates the header file and adds a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ * @a: parameter one
+ * @b: parameter two
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
