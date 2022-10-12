bc0c9e43005f1e131c2399b5798c5a739af503aa
JoshuaAkpan <joshuaakpan7@gmail.com>
2022-10-11 18:13:28 -0700

    This commit adds a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRUCT
+#define STRUCT
+/**
+ * struct dog - describes a dog specie
+ *
+ * @name: The dog name
+ * @age: The dog age
+ * @owner: The dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
