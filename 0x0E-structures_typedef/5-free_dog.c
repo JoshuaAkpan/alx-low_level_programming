b705308a13e127b88e1d9bb1ae09e66c02b7745f
JoshuaAkpan <joshuaakpan7@gmail.com>
2022-10-11 20:35:37 -0700

    This commit adds a function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog structure
+ * @d: pointer to the structure
+ * Return: Nil
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
