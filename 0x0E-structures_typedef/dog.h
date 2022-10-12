062a86156ce41a62d12eeb0c133cb3ded1b6f03d
JoshuaAkpan <joshuaakpan7@gmail.com>
2022-10-11 19:21:47 -0700

    header file updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,12 @@ struct dog
 };
 
 /**
- * init_dog - a function
+ *init_dog - a function
+ *@d: a pointer to the struct type
+ *@name: the dog name
+ *@age: the dog age
+ *@owner: the dog owner
+ *Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
